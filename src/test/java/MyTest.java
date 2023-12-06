b07c595fc7819072be8ae19172f156aba57a9a45
Pasunuri Srinidhi <93040752+PasunuriSrinidhi@users.noreply.github.com>
2023-12-06 14:34:30 +0530

    created custom exceptions and included in testing

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -4,6 +4,12 @@ import java.util.regex.Pattern;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import com.example.Validator;
+import com.example.Exceptions.InvalidEmailException;
+import com.example.Exceptions.InvalidFirstNameException;
+import com.example.Exceptions.InvalidPasswordException;
+import com.example.Exceptions.InvalidPhoneNumberException;
+
 public class MyTest {
 
     // UC11: testing with all given email ids
@@ -18,6 +24,17 @@ public class MyTest {
             "abc@1.com",
             "abc@gmail.com.com",
             "abc+100@gmail.com",
+    })
+    public void testEmailHappy(String email) {
+        String emailRegex = "^+[a-zA-Z0-9$&%_+-]+(\\.[a-zA-Z0-9$&%_+-]+)?@[a-zA-Z0-9-]+(\\.[a-zA-Z]{2,}){1,2}+$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+        assertTrue(matcher.matches());
+    }
+
+    // UC12: handle custom exceptions for sad test cases
+    @ParameterizedTest
+    @ValueSource(strings = {
             "abc",
             "abc@.com.my",
             "abc123@gmail.a",
@@ -32,39 +49,76 @@ public class MyTest {
             "abc@gmail.com.1a",
             "abc@gmail.com.aa.au"
     })
-    public void testEmails(String email) {
-        String emailRegex = "^[a-zA-Z0-9.$&%_+-]+@[a-zA-Z0-9-]+(\\.[a-zA-Z]{2,})+$";
-        Pattern pattern = Pattern.compile(emailRegex);
-        Matcher matcher = pattern.matcher(email);
-        assertTrue(matcher.matches());
-    
-}
-    
-   
+    public void testEmailSad(String email) throws InvalidEmailException {
+        try {
+            Validator.validateEmail(email);
+            fail("Expected InvalidEmailException");
+        } catch (InvalidEmailException exception) {
+            assertEquals("Email is invalid", exception.getMessage());
+        }
+    }
+
+    // UC10: testing all other fields
     @ParameterizedTest
-    @ValueSource(strings = { "Srinidhi", "srinidhi", "ab", "aB", "abc", "Abc" })
-    public void testNames(String name) {
+    @ValueSource(strings = { "Chinmay", "Abc" })
+    public void testNameHappy(String name) {
         String nameRegex = "^[A-Z][a-zA-Z]{2,}$";
         Pattern pattern = Pattern.compile(nameRegex);
         Matcher matcher = pattern.matcher(name);
         assertTrue(matcher.matches());
     }
 
+    // UC12: handle custom exceptions for sad test cases
     @ParameterizedTest
-    @ValueSource(strings = { "91 9313402393", "+91 9313402393", "9313402393", "93134 02393", "919313402393" })
-    public void testPhoneNumbers(String phone) {
+    @ValueSource(strings = { "srinidhi", "ab", "aB", "abc" })
+    public void testNameSad(String name) throws InvalidFirstNameException {
+        try {
+            Validator.validateFirstName(name);
+            fail("Expected InvalidFirstNameException");
+        } catch (InvalidFirstNameException exception) {
+            assertEquals("First name is invalid", exception.getMessage());
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "91 9313402393", "99 8412612" })
+    public void testPhoneNumberHappy(String phone) {
         String phoneRegex = "^[0-9]{1,4} [0-9]{5,15}$";
         Pattern pattern = Pattern.compile(phoneRegex);
         Matcher matcher = pattern.matcher(phone);
         assertTrue(matcher.matches());
     }
 
+    // UC12: handle custom exceptions for sad test cases
     @ParameterizedTest
-    @ValueSource(strings = { "Hello", "helloworld", "HelloWorld", "HelloWorld123", "HelloWorld@123" })
-    public void testPassword(String pasword) {
+    @ValueSource(strings = { "+91 9313402393", "9313402393", "93134 02393", "919313402393" })
+    public void testPhoneNumberSad(String phone) throws InvalidPhoneNumberException {
+        try {
+            Validator.validatePhoneNumber(phone);
+            fail("Expected InvalidPhoneNumberException");
+        } catch (InvalidPhoneNumberException exception) {
+            assertEquals("Phone number is invalid", exception.getMessage());
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "HelloWorld@123", "IamPassword#05" })
+    public void testPasswordHappy(String pasword) {
         String passwordRegex = "^(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z\\d]).{8,}$";
         Pattern pattern = Pattern.compile(passwordRegex);
         Matcher matcher = pattern.matcher(pasword);
         assertTrue(matcher.matches());
+    }
+
+    // UC12: handle custom exceptions for sad test cases
+    @ParameterizedTest
+    @ValueSource(strings = { "Hello", "helloworld", "HelloWorld", "HelloWorld123" })
+    public void testPasswordSad(String pasword) throws InvalidPasswordException {
+        try {
+            Validator.validatePassword(pasword);
+            fail("Expected InvalidPasswordException");
+        } catch (InvalidPasswordException exception) {
+            assertEquals("Password is invalid", exception.getMessage());
+        }
     }
 }
